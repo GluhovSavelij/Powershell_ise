81eabf5923236b5e0c54230251d86fe8a267e271
GluhovSavelij <gluhovcavelij@gmail.com>
2025-12-24 16:13:45 +0300

    network_to_duplex.ps1

--- network_to_duplex.ps1
+++ network_to_duplex.ps1
@@ -1,9 +1,11 @@
-$bytes1251 = $encoding1251.GetBytes($originalText)
-$originalText = 
+$bytes1251_1 = $encoding1251.GetBytes($originalText1)
+$bytes1251_2 = $encoding1251.GetBytes($originalText2)
+$original = "Скорость и дуплекс"
+$original = "1 Гбит/с дуплекс" 
 Get-NetAdapter | Where-Object { $_.LinkSpeed -eq '100 Mbps' } | ForEach-Object {
 IF($_.Name -like '*hernet*'  )
 {
-    Set-NetAdapterAdvancedProperty -Name $_.Name -DisplayName "Скорость и дуплекс" -DisplayValue "1 Гбит/с дуплекс"  
+    Set-NetAdapterAdvancedProperty -Name $_.Name -DisplayName $original1 -DisplayValue $original2
     Restart-NetAdapter -Name $_.Name 
 }
 }
