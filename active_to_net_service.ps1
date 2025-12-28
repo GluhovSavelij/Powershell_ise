617ddf6b427a4d95d2b820121e8b3341bea76cd8
GluhovSavelij <gluhovcavelij@gmail.com>
2025-12-28 13:24:34 +0300

    Update active_to_net_service.ps1

--- active_to_net_service.ps1
+++ active_to_net_service.ps1
@@ -1,4 +1,4 @@
-@('Dnscache', 'Dhcp', 'LanmanServer', 'Browser', 'Netlogon', 'SamSs', 'LanmanWorkstation', 'KeyIso', 'FDResPub', 'SSDPSRV', 'upnphost', 'RpcSs', 'RpcEptMapper', 'EventLog', 'lmhosts', 'NlaSvc') | ForEach-Object { 
+@('SessionEnv','WebClient','ClipSVC','HgClientService','Dnscache', 'Dhcp', 'LanmanServer', 'Browser', 'Netlogon', 'SamSs', 'LanmanWorkstation', 'KeyIso', 'FDResPub', 'SSDPSRV', 'upnphost', 'RpcSs', 'RpcEptMapper', 'EventLog', 'lmhosts', 'NlaSvc') | ForEach-Object { 
     Start-Service -Name $_ 
 }
 # Покажем все службы связанные с сетью
