b39b161e8c9a3c1254edd5e97bb3829a6239dba2
GluhovSavelij <gluhovcavelij@gmail.com>
2025-12-28 13:18:15 +0300

    active_to_net_service.ps1

--- active_to_net_service.ps1
+++ active_to_net_service.ps1
@@ -1,3 +1,13 @@
 @('Dnscache', 'Dhcp', 'LanmanServer', 'Browser', 'Netlogon', 'SamSs', 'LanmanWorkstation', 'KeyIso', 'FDResPub', 'SSDPSRV', 'upnphost', 'RpcSs', 'RpcEptMapper', 'EventLog', 'lmhosts', 'NlaSvc') | ForEach-Object { 
     Start-Service -Name $_ 
 }
+# Покажем все службы связанные с сетью
+Get-Service | Where-Object {
+    $_.DisplayName -like "*сеть*" -or 
+    $_.DisplayName -like "*сервер*" -or
+    $_.DisplayName -like "*клиент*" -or
+    $_.DisplayName -like "*обозреватель*" -or
+    $_.DisplayName -like "*обнаруж*" -or
+    $_.DisplayName -like "*SMB*" -or
+    $_.DisplayName -like "*NetBIOS*"
+} | Select-Object Name, DisplayName, Status | Format-Table -AutoSize
